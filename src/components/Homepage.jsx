40544ce47cc6c7c74ef8a42adc099f0297785ad7
Nicole Raymond <nicoleraymond@hotmail.com>
2025-05-22 12:03:27 +0100

    adds topic filter

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,21 +1,26 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import ArticleList from "./ArticleList";
+import Header from "../Header";
 
 function Homepage() {
   const [articles, setArticles] = useState([]);
+  const [selectedTopic, setSelectedTopic] = useState("");
 
   useEffect(() => {
-    axios
-      .get("https://northcoders-news-be-s4h6.onrender.com/api/articles")
-      .then(({data}) => {
-        setArticles(data.articles);
-      });
-  }, []);
+    let url = "https://northcoders-news-be-s4h6.onrender.com/api/articles";
+    if (selectedTopic) {
+      url += `?topic=${selectedTopic}`;
+    }
+    axios.get(url).then(({ data }) => {
+      setArticles(data.articles);
+    });
+  }, [selectedTopic]);
 
   return (
     <div>
-      <ArticleList articles={articles}/>
+      <Header selectedTopic={selectedTopic} onTopicChange={setSelectedTopic} />
+      <ArticleList articles={articles} />
     </div>
   );
 }
