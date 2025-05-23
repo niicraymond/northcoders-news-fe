87fb638e6277617033fa93ccc2ac8ec206cd4655
Nicole Raymond <nicoleraymond@hotmail.com>
2025-05-23 14:28:42 +0100

    updates voting

--- src/components/VotingSection.jsx
+++ src/components/VotingSection.jsx
@@ -1,16 +1,25 @@
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import { Box, Button, Typography } from "@mui/material";
+import { UserContext } from "../contexts/UserContext";
 
 function VotingSection({ initialVotes, articleId }) {
+  const { user } = useContext(UserContext);
   const [vote, setVote] = useState(initialVotes);
+  const [hasVoted, setHasVoted] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     setVote(initialVotes);
   }, [initialVotes]);
 
   const handleVote = (amount) => {
-    setVote((prev) => prev + amount);
+    if (!user || hasVoted || isLoading) setVote((prev) => prev + amount);
+    setHasVoted(true);
+    setIsLoading(true);
+    setError(null);
+
     axios
       .patch(
         `https://northcoders-news-be-s4h6.onrender.com/api/articles/${articleId}`,
@@ -20,6 +29,14 @@ function VotingSection({ initialVotes, articleId }) {
       )
       .then(({ data }) => {
         setVote(data.article.votes);
+      })
+      .catch(() => {
+        setHasVoted(false);
+        setVote(initialVotes);
+        setError("Vote failed");
+      })
+      .finally(() => {
+        setIsLoading(false);
       });
   };
 
@@ -29,19 +46,26 @@ function VotingSection({ initialVotes, articleId }) {
         onClick={() => {
           handleVote(1);
         }}
+        disabled={!user || hasVoted || isLoading}
       >
         👍
       </Button>
-      <Typography color="text.primary">
-        {vote}
-      </Typography>
+      <Typography color="text.primary">{vote}</Typography>
       <Button
         onClick={() => {
           handleVote(-1);
         }}
+        disabled={!user || hasVoted || isLoading}
       >
         👎
       </Button>
+      <Box>
+        {error && (
+          <Typography variant="caption" color="error">
+            {error}
+          </Typography>
+        )}
+      </Box>
     </Box>
   );
 }
