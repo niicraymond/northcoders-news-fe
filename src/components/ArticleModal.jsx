c6ade3b8b7c7752dc3247d63a9a3fa849f89c75b
Nicole Raymond <nicoleraymond@hotmail.com>
2025-08-01 13:52:03 +0100

    updates modal styling

--- src/components/ArticleModal.jsx
+++ src/components/ArticleModal.jsx
@@ -1,4 +1,11 @@
-import { Box, Typography, Modal, CardMedia, IconButton } from "@mui/material";
+import {
+  Box,
+  Typography,
+  Modal,
+  CardMedia,
+  IconButton,
+  Divider,
+} from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 import axios from "axios";
 import { useState, useEffect } from "react";
@@ -40,8 +47,7 @@ function ArticleModal({ onClose, articleId }) {
   };
 
   return (
-    <Modal open={true}>
-      {}
+    <Modal open={true} onClose={onClose} aria-labelledby="article-modal-title">
       <Box sx={style}>
         <IconButton
           onClick={onClose}
@@ -50,36 +56,105 @@ function ArticleModal({ onClose, articleId }) {
         >
           <CloseIcon />
         </IconButton>
-        <Typography variant="h6" gutterBottom sx={{ color: "black" }}>
-          {article.title}
-        </Typography>
-        <Typography variant="body2" gutterBottom sx={{ color: "black" }}>
-          Author: {article.author} Topic: {article.topic}
-        </Typography>
-        <Typography variant="body1" gutterBottom sx={{ color: "black" }}>
+
+        <Box sx={{ textAlign: "center" }}>
+          <Typography
+            id="article-modal-title"
+            variant="h5"
+            gutterBottom
+            sx={{
+              color: "black",
+              fontWeight: 600,
+              position: "relative",
+              lineHeight: 1.1,
+              "&::after": {
+                content: '""',
+                display: "block",
+                width: 60,
+                height: 4,
+                bgcolor: "#646cff",
+                borderRadius: 2,
+                mt: 1,
+                mx: "auto",
+              },
+            }}
+          >
+            {article.title}
+          </Typography>
+          <Typography
+            variant="body2"
+            gutterBottom
+            sx={{
+              color: "black",
+              textAlign: "center",
+              fontSize: "0.9rem",
+              fontWeight: 500,
+              letterSpacing: 0.5,
+              mt: 1,
+            }}
+          >
+            Author: {article.author} &bull; Topic: {article.topic}
+          </Typography>
+        </Box>
+
+        <Typography
+          variant="body1"
+          gutterBottom
+          sx={{
+            color: "black",
+            whiteSpace: "pre-line",
+            lineHeight: 1.6,
+          }}
+        >
           {article.body}
         </Typography>
-        {/* <Typography variant="body2" gutterBottom>{article.created_at}</Typography> */}
-        <Box>
-          <CardMedia
-            component="img"
-            image={article.article_img_url}
-            title={article.title}
-            sx={{
-              width: "100%",
-              maxHeight: 300,
-              aspectRatio: "16/9",
-              objectFit: "cover",
-              borderRadius: 1.5,
-              backgroundColor: "rgba(0,0,0,0.04)",
-            }}
-          />
+
+        {article.article_img_url && (
+          <Box>
+            <CardMedia
+              component="img"
+              image={article.article_img_url}
+              title={article.title}
+              sx={{
+                width: "100%",
+                aspectRatio: "16/9",
+                objectFit: "cover",
+                borderRadius: 1.5,
+                backgroundColor: "rgba(0,0,0,0.04)",
+              }}
+            />
+          </Box>
+        )}
+
+        <Box
+          sx={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "space-between",
+            alignItems: "center",
+            gap: 2,
+            width: "100%",
+            mt: 1,
+            minWidth: 0,
+          }}
+        >
+          <Box sx={{ minWidth: 0 }}>
+            <VotingSection articleId={articleId} initialVotes={article.votes} />
+          </Box>
+          <Typography
+            variant="body2"
+            gutterBottom
+            sx={{ color: "black", minWidth: 0 }}
+          >
+            Comments: {article.comment_count}
+          </Typography>
         </Box>
-        <VotingSection articleId={articleId} initialVotes={article.votes} />
-        <Typography variant="body2" gutterBottom sx={{ color: "black" }}>
-          Comments: {article.comment_count}
-        </Typography>
-        <CommentSection articleId={articleId} />
+
+        <Divider sx={{ my: 2 }} />
+
+        <Box sx={{ width: "100%", minWidth: 0 }}>
+          <CommentSection articleId={articleId} />
+        </Box>
       </Box>
     </Modal>
   );
